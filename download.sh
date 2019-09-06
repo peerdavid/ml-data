edfde9617c1b174c8bd1f32c13b9c109d25a93ec
Peer David <david.peer.1991@gmail.com>
2019-09-06 13:58:40 +0200

    Added download of svhn dataset

--- download.sh
+++ download.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-
-#
 # smallNORB
-#
 DATADIR="./data/smallNORB/mat"
 mkdir -p "./data/smallNORB/tfrecord"
 mkdir -p $DATADIR
@@ -20,16 +17,12 @@ for gzfile in $DATADIR/*.gz ; do
 done
 
 
-#
 # MNIST
-#
 mkdir -p "./data/mnist/tfrecord"
 mkdir -p "./data/mnist/ubyte"
 
 
-#
 # Fashion MNIST
-#
 DATADIR="./data/fashionMNIST/ubyte"
 mkdir -p $DATADIR
 mkdir -p "./data/fashionMNIST/tfrecord"
@@ -42,3 +35,16 @@ done < "./url_fashion.txt"
 echo "fashionMNIST: Done fetching archive files. Extracting..."
 
 echo "Done!"
+
+
+# svhn
+DATADIR="./data/svhn/mat"
+mkdir -p "./data/svhn/tfrecord"
+mkdir -p $DATADIR
+
+while read url ; do
+    echo "fetching $url"
+    wget -P "$DATADIR/" "$url"
+done < "./url_svhn.txt"
+
+echo "svhn: Done fetching files. Extracting..."
